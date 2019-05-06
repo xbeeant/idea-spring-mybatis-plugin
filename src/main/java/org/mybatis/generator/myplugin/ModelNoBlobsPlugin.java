9a7829ff1190b5fe6a7cecc48046da4d9f4c69af
Beeant <huangxb0512@gmail.com>
2019-05-06 13:01:30 +0800

    Update ModelNoBlobsPlugin.java

--- src/main/java/org/mybatis/generator/myplugin/ModelNoBlobsPlugin.java
+++ src/main/java/org/mybatis/generator/myplugin/ModelNoBlobsPlugin.java
@@ -4,11 +4,16 @@ import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.PluginAdapter;
 import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
 import org.mybatis.generator.api.dom.java.JavaVisibility;
 import org.mybatis.generator.api.dom.java.TopLevelClass;
+import org.mybatis.generator.api.dom.xml.Attribute;
+import org.mybatis.generator.api.dom.xml.XmlElement;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author xiaobiao
@@ -30,7 +35,7 @@ public class ModelNoBlobsPlugin extends PluginAdapter {
         }
 
         for (IntrospectedColumn blobColumn : blobColumns) {
-            if(existField.contains(blobColumn.getJavaProperty())){
+            if (existField.contains(blobColumn.getJavaProperty())) {
                 continue;
             }
             Field field = new Field(blobColumn.getJavaProperty(), blobColumn.getFullyQualifiedJavaType());
@@ -44,7 +49,68 @@ public class ModelNoBlobsPlugin extends PluginAdapter {
     }
 
     @Override
+    public boolean sqlMapResultMapWithBLOBsElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
+        removeBlobs(element);
+        return super.sqlMapResultMapWithBLOBsElementGenerated(element, introspectedTable);
+    }
+
+    private void removeBlobs(XmlElement element) {
+        List<Attribute> attributes = element.getAttributes();
+        Iterator<Attribute> iterator = attributes.iterator();
+        while (iterator.hasNext()) {
+            Attribute next = iterator.next();
+            if (next.getValue().contains("WithBLOBs") && ("type".equals(next.getName()) || "parameterType".equals(next.getName()))) {
+                next.setValue(next.getValue().replace("WithBLOBs", ""));
+            }
+        }
+    }
+
+    @Override
     public boolean modelRecordWithBLOBsClassGenerated(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
         return false;
+    }
+
+    @Override
+    public boolean sqlMapSelectByExampleWithBLOBsElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
+        removeBlobs(element);
+        return super.sqlMapSelectByExampleWithBLOBsElementGenerated(element, introspectedTable);
+    }
+
+    @Override
+    public boolean sqlMapDeleteByPrimaryKeyElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
+        removeBlobs(element);
+        return super.sqlMapDeleteByPrimaryKeyElementGenerated(element, introspectedTable);
+    }
+
+    @Override
+    public boolean sqlMapSelectByPrimaryKeyElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
+        removeBlobs(element);
+        return super.sqlMapSelectByPrimaryKeyElementGenerated(element, introspectedTable);
+    }
+
+    @Override
+    public boolean sqlMapUpdateByPrimaryKeySelectiveElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
+        removeBlobs(element);
+        return super.sqlMapUpdateByPrimaryKeySelectiveElementGenerated(element, introspectedTable);
+    }
+
+    @Override
+    public boolean sqlMapInsertSelectiveElementGenerated(XmlElement element, IntrospectedTable introspectedTable) {
+        removeBlobs(element);
+        return super.sqlMapInsertSelectiveElementGenerated(element, introspectedTable);
+    }
+
+    @Override
+    public boolean modelFieldGenerated(Field field, TopLevelClass topLevelClass, IntrospectedColumn introspectedColumn, IntrospectedTable introspectedTable, ModelClassType modelClassType) {
+        Set<FullyQualifiedJavaType> importedTypes = topLevelClass.getImportedTypes();
+        Iterator<FullyQualifiedJavaType> iterator = importedTypes.iterator();
+        while (iterator.hasNext()) {
+            FullyQualifiedJavaType next = iterator.next();
+            if (next.getFullyQualifiedName().endsWith("WithBLOBs")) {
+                iterator.remove();
+            }
+        }
+
+        return super.modelFieldGenerated(field, topLevelClass, introspectedColumn, introspectedTable, modelClassType);
     }
 }
