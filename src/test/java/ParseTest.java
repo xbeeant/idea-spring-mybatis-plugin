80cb692569cbbbfe66db734e93102f1ce1ea5c8e
黄小标 <huangxb0512@gmail.com>
2020-11-30 10:10:46 +0800

    new ui & code merger

--- src/test/java/ParseTest.java
+++ src/test/java/ParseTest.java
@@ -1,5 +1,5 @@
-import com.freetmp.mbg.merge.CompilationUnitMerger;
 import com.github.javaparser.ParseException;
+import com.xstudio.javamerge.merger.CompilationUnitMerger;
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -15,16 +15,16 @@ public class ParseTest {
 
     @Test
     public void parseObject() throws IOException, ParseException {
-        // /Users/ca62785/Codes/xstudio-java-merge/src/test/java/com/xstudio/test/A.java
-        URL resource = ParseTest.class.getResource("");
-        String path = resource.getPath();
-        String sourcePathParent = path.substring(0, path.indexOf("/build"));
-        File afile = new File("/Users/ca62785/Codes/xstudio-java-merge/src/test/java/com/xstudio/test/object/first/FirstObjectWithAnnotation.java");
-        File bfile = new File("/Users/ca62785/Codes/xstudio-java-merge/src/test/java/com/xstudio/test/object/second/SecondObject.java");
-        String merge = CompilationUnitMerger.merge(
-                FileUtils.readFileToString(afile, Charset.defaultCharset()),
-                FileUtils.readFileToString(bfile, Charset.defaultCharset())
-        );
-        logger.info("merge result: \n{}", merge);
+//        // /Users/ca62785/Codes/xstudio-java-merge/src/test/java/com/xstudio/test/A.java
+//        URL resource = ParseTest.class.getResource("");
+//        String path = resource.getPath();
+//        String sourcePathParent = path.substring(0, path.indexOf("/build"));
+//        File afile = new File("/Users/ca62785/Codes/xstudio-java-merge/src/test/java/com/xstudio/test/object/first/FirstObjectWithAnnotation.java");
+//        File bfile = new File("/Users/ca62785/Codes/xstudio-java-merge/src/test/java/com/xstudio/test/object/second/SecondObject.java");
+//        String merge = CompilationUnitMerger.merge(
+//                FileUtils.readFileToString(afile, Charset.defaultCharset()),
+//                FileUtils.readFileToString(bfile, Charset.defaultCharset())
+//        );
+//        logger.info("merge result: \n{}", merge);
     }
 }
